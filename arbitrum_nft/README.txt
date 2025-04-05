f13e7cdd8e09236753f531f46f14e4e13d4a0896
0xEureka <qhcgyy@gmail.com>
2025-04-05 17:39:49 +0800

    update README; update arbitrum_nft; add podfun.html and learnmore.html

--- arbitrum_nft/README.txt
+++ arbitrum_nft/README.txt
@@ -1,11 +1,14 @@
 https://www.hackquest.io/learn/b99cca2c-679a-4241-ab30-f10069f5457e/19939e3f-d060-445b-b016-344f26e9a514
+#TODO: Error on cargo stylus export-abi
 
 rustup default 1.81
 rustup target add wasm32-unknown-unknown --toolchain 1.81
 
 cargo stylus new <YOUR_PROJECT_NAME>
 
-cargo stylus check -e https://sepolia-rollup.arbitrum.io/rpc
+source .env
+
+cargo stylus check -e $ARBITRUM_TESTNET_RPC
 cargo stylus deploy -e $ARBITRUM_TESTNET_RPC --private-key=$PRIVATE_KEY --estimate-gas
 cargo stylus deploy -e $ARBITRUM_TESTNET_RPC --private-key=$PRIVATE_KEY --estimate-gas --no-verify
 cargo stylus deploy -e $ARBITRUM_TESTNET_RPC --private-key=$PRIVATE_KEY --no-verify
--- arbitrum_nft/src/erc721.rs
+++ arbitrum_nft/src/erc721.rs
@@ -1,11 +1,15 @@
+use alloc::{string::String, vec::Vec};
+use alloy_primitives::FixedBytes;
 use core::marker::PhantomData;
 use stylus_sdk::{
     alloy_primitives::{Address, U256},
-    alloy_sol_types::sol,
+    alloy_sol_types::{sol},
     call::Call,
-    msg,
+    evm, msg,
     prelude::*,
 };
+
+use stylus_sdk::call::MethodError;
 
 pub trait Erc721Params {
     const NAME: &'static str;
@@ -14,14 +18,20 @@ pub trait Erc721Params {
 
 sol_storage! {
     pub struct Erc721<T> {
+        /// Token id to owner map
         mapping(uint256 => address) _owners;
+        /// User to balance map
         mapping(address => uint256) _balances;
+        /// Token id to approved user map
         mapping(uint256 => address) _approvals;
+        /// User to operator map (the operator can manage all NFTs of the owner.)
         mapping(address => mapping(address => bool)) _approvals_for_all;
+        /// Used to allow [`Erc721Params`]
         PhantomData<T> phantom;
     }
 }
 
+// Declare events and Solidity error types
 sol! {
     event Transfer(address indexed from, address indexed to, uint256 indexed token_id);
     event Approval(address indexed owner, address indexed spender, uint256 indexed token_id);
@@ -36,13 +46,12 @@ sol! {
     error CallFailed();
 }
 
-sol! {
+sol_interface! {
     interface IERC721TokenReceiver {
-        function onERC721Received(address operator, address from, uint256 token_id, bytes calldata data) external returns(bytes4);
+        function onERC721Received(address operator, address from, uint256 token_id, bytes data) external returns(bytes4);
     }
 }
 
-#[derive(SolidityError)]
 pub enum Erc721Error {
     NotOwner(NotOwner),
     NotAuthorized(NotAuthorized),
@@ -53,53 +62,265 @@ pub enum Erc721Error {
     CallFailed(CallFailed),
 }
 
+impl From<Erc721Error> for Vec<u8> {
+    fn from(err: Erc721Error) -> Vec<u8> {
+        match err {
+            Erc721Error::NotOwner(e) => e.encode(),
+            Erc721Error::NotAuthorized(e) => e.encode(),
+            Erc721Error::InvalidRecipient(e) => e.encode(),
+            Erc721Error::AlreadyMinted(e) => e.encode(),
+            Erc721Error::NotMinted(e) => e.encode(),
+            Erc721Error::UnsafeRecipient(e) => e.encode(),
+            Erc721Error::CallFailed(e) => e.encode(),
+        }
+    }
+}
+
+const ADDRESS_ZERO: Address = Address(FixedBytes([0u8; 20]));
+const ERC721_TOKEN_RECEIVER_ID: u32 = 0x150b7a02;
+
+// These methods are external to other contracts
 #[public]
 impl<T: Erc721Params> Erc721<T> {
-    pub fn _mint(&mut self, to: Address, token_id: U256) -> Result<(), Erc721Error> {
-        if to.is_zero() {
-            return Err(Erc721Error::InvalidRecipient(InvalidRecipient { to }));
+    pub fn name() -> Result<String, Vec<u8>> {
+        Ok(T::NAME.into())
+    }
+
+    pub fn symbol() -> Result<String, Vec<u8>> {
+        Ok(T::SYMBOL.into())
+    }
+
+    pub fn balance_of(&self, owner: Address) -> Result<U256, Erc721Error> {
+        Ok(self._balances.get(owner))
+    }
+
+    pub fn owner_of(&self, token_id: U256) -> Result<Address, Erc721Error> {
+        Ok(self._owners.get(token_id))
+    }
+
+    pub fn get_approved(&self, token_id: U256) -> Result<Address, Erc721Error> {
+        Ok(self._approvals.get(token_id))
+    }
+
+    pub fn is_approved_for_all(
+        &self,
+        owner: Address,
+        operator: Address,
+    ) -> Result<bool, Erc721Error> {
+        Ok(self._approvals_for_all.get(owner).get(operator))
+    }
+
+    pub fn approve(&mut self, spender: Address, token_id: U256) -> Result<(), Erc721Error> {
+        // address owner = _ownerOf[id];
+        let owner = self._owners.getter(token_id).get();
+
+        // require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "NOT_AUTHORIZED");
+        if msg::sender() != owner && !self._approvals_for_all.get(owner).get(msg::sender()) {
+            return Err(Erc721Error::NotOwner(NotOwner {
+                account: owner,
+                token_id,
+            }));
         }
 
-        if self._owners.get(token_id) != Address::ZERO {
-            return Err(Erc721Error::AlreadyMinted(AlreadyMinted { token_id }));
-        }
+        // getApproved[id] = spender;
+        let mut spender_of = self._approvals.setter(token_id);
+        spender_of.set(spender);
 
-        self._balances.insert(to, self._balances.get(to) + U256::from(1));
-        self._owners.insert(token_id, to);
-        
+        // emit Approval(owner, spender, id);
+        evm::log(Approval {
+            owner,
+            spender,
+            token_id,
+        });
+
         Ok(())
     }
 
-    pub fn _safe_mint(&mut self, to: Address, token_id: U256) -> Result<(), Erc721Error> {
-        self._mint(to, token_id)?;
+    pub fn set_approval_for_all(
+        &mut self,
+        operator: Address,
+        approved: bool,
+    ) -> Result<(), Erc721Error> {
+        // isApprovedForAll[msg.sender][operator] = approved;
+        let mut operator_setter = self._approvals_for_all.setter(msg::sender());
+        let mut approval_setter = operator_setter.setter(operator);
+        approval_setter.set(approved);
 
-        if stylus_sdk::contract::is_contract(to) {
-            let data = Vec::new();
-            let selector = IERC721TokenReceiver::onERC721ReceivedCall::SELECTOR;
-            let result = Call::new()
-                .call_selector(selector)
-                .dest(to)
-                .args((msg::sender(), Address::ZERO, token_id, data))
-                .call()
-                .map_err(|_| Erc721Error::CallFailed(CallFailed {}))?;
+        // emit ApprovalForAll(msg.sender, operator, approved);
+        evm::log(ApprovalForAll {
+            owner: msg::sender(),
+            operator,
+            approved,
+        });
 
-            if !result.as_slice().starts_with(&selector) {
-                return Err(Erc721Error::UnsafeRecipient(UnsafeRecipient { recipient: to }));
-            }
+        Ok(())
+    }
+
+    pub fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), Erc721Error> {
+        // require(from == _ownerOf[id], "WRONG_FROM");
+        let mut owner_of_id = self._owners.setter(token_id);
+        if owner_of_id.get() != from {
+            return Err(Erc721Error::NotOwner(NotOwner {
+                account: from,
+                token_id,
+            }));
         }
+
+        // require(to != address(0), "INVALID_RECIPIENT");
+        if to == ADDRESS_ZERO {
+            return Err(Erc721Error::InvalidRecipient(InvalidRecipient { to }));
+        }
+
+        // require(msg.sender == from || isApprovedForAll[from][msg.sender] || msg.sender == getApproved[id], "NOT_AUTHORIZED");
+        if msg::sender() != from
+            && self._approvals_for_all.get(from).get(msg::sender())
+            && msg::sender() != self._approvals.get(token_id)
+        {
+            return Err(Erc721Error::NotAuthorized(NotAuthorized {
+                caller: msg::sender(),
+            }));
+        }
+
+        // _balanceOf[from]--;
+        let mut from_balance = self._balances.setter(from);
+        let new_from_balance = from_balance.get() - U256::from(1);
+        from_balance.set(new_from_balance);
+
+        // _balanceOf[to]++;
+        let mut to_balance = self._balances.setter(to);
+        let new_to_balance = to_balance.get() + U256::from(1);
+        to_balance.set(new_to_balance);
+
+        // _ownerOf[id] = to;
+        owner_of_id.set(to);
+
+        // delete getApproved[id];
+        self._approvals.setter(token_id).set(ADDRESS_ZERO);
+
+        evm::log(Transfer { from, to, token_id });
+
+        Ok(())
+    }
+
+    pub fn safe_transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), Erc721Error> {
+        // transferFrom(from, to, id);
+        self.transfer_from(from, to, token_id)?;
+
+        self._check_recipient_is_valid(from, to, token_id)?;
+
+        Ok(())
+    }
+
+    pub fn supports_interface(interface: [u8; 4]) -> Result<bool, Vec<u8>> {
+        let supported = interface == 0x01ffc9a7u32.to_be_bytes() // ERC165 Interface ID for ERC165
+            || interface == 0x80ac58cdu32.to_be_bytes() // ERC165 Interface ID for ERC721
+            || interface == 0x780e9d63u32.to_be_bytes(); // ERC165 Interface ID for ERC721Metadata
+        Ok(supported)
+    }
+}
+
+// internal mint+burn methods
+impl<T: Erc721Params> Erc721<T> {
+    pub fn _mint(&mut self, to: Address, token_id: U256) -> Result<(), Erc721Error> {
+        // require(to != address(0), "INVALID_RECIPIENT");
+        if to == ADDRESS_ZERO {
+            return Err(Erc721Error::InvalidRecipient(InvalidRecipient { to }));
+        }
+
+        // require(_ownerOf[id] == address(0), "ALREADY_MINTED");
+        let mut owner_of_id = self._owners.setter(token_id);
+        if owner_of_id.get() != ADDRESS_ZERO {
+            return Err(Erc721Error::AlreadyMinted(AlreadyMinted { token_id }));
+        }
+
+        // _balanceOf[to]++;
+        let mut to_balance = self._balances.setter(to);
+        let new_to_balance = to_balance.get() + U256::from(1);
+        to_balance.set(new_to_balance);
+
+        // _ownerOf[id] = to;
+        owner_of_id.set(to);
+
+        // emit Transfer(address(0), to, id);
+        evm::log(Transfer {
+            from: ADDRESS_ZERO,
+            to,
+            token_id,
+        });
 
         Ok(())
     }
 
     pub fn _burn(&mut self, token_id: U256) -> Result<(), Erc721Error> {
-        let owner = self._owners.get(token_id);
-        if owner == Address::ZERO {
+        // address owner = _ownerOf[id];
+        let mut owner = self._owners.setter(token_id);
+
+        // require(owner != address(0), "NOT_MINTED");
+        if owner.get() == ADDRESS_ZERO {
             return Err(Erc721Error::NotMinted(NotMinted { token_id }));
         }
 
-        self._balances.insert(owner, self._balances.get(owner) - U256::from(1));
-        self._owners.insert(token_id, Address::ZERO);
-        
+        // _balanceOf[owner]--;
+        let mut owner_balance = self._balances.setter(owner.get());
+        let new_owner_balance = owner_balance.get() - U256::from(1);
+        owner_balance.set(new_owner_balance);
+
+        // delete _ownerOf[id];
+        owner.set(ADDRESS_ZERO);
+
+        // delete getApproved[id];
+        self._approvals.setter(token_id).set(ADDRESS_ZERO);
+
+        // emit Transfer(owner, address(0), id);
+        evm::log(Transfer {
+            from: owner.get(),
+            to: ADDRESS_ZERO,
+            token_id,
+        });
+
+        Ok(())
+    }
+
+    pub fn _safe_mint(&mut self, to: Address, token_id: U256) -> Result<(), Erc721Error> {
+        // _mint(to, id);
+        self._mint(to, token_id)?;
+
+        self._check_recipient_is_valid(ADDRESS_ZERO, to, token_id)?;
+
+        Ok(())
+    }
+
+    pub fn _check_recipient_is_valid(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), Erc721Error> {
+        // require(to.code.length == 0 || ERC721TokenReceiver(to).onERC721Received(msg.sender, from, id, "") == ERC721TokenReceiver.onERC721Received.selector, "UNSAFE_RECIPIENT");
+        if to.has_code() {
+            let receiver = IERC721TokenReceiver::new(to);
+            let config = Call::new();
+            let hook_result = receiver
+                .on_erc_721_received(config, msg::sender(), from, token_id, vec![].into())
+                .map_err(|_e| Erc721Error::CallFailed(CallFailed {}))?;
+
+            if u32::from_be_bytes(hook_result.0) != ERC721_TOKEN_RECEIVER_ID {
+                return Err(Erc721Error::UnsafeRecipient(UnsafeRecipient {
+                    recipient: to,
+                }));
+            }
+        }
+
         Ok(())
     }
 }
